fbde931fea8ddad71283fce59603adeaa5007eed
Dcbp <danielpucci.8@gmail.com>
2024-04-25 13:17:36 -0300

    Animations Home - global animation all + fix

--- assets/js/gsap/trigger.js
+++ assets/js/gsap/trigger.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.set('.anm_heading_title_1', {
         opacity: 1,        
     });
-    gsap.set('.anm_heading_title_2', {
-        opacity: 1,
-        y: `${+innerHeight}px`
-    });
+    // gsap.set('.anm_heading_title_2', {
+    //     opacity: 1,
+    //     y: `${+innerHeight}px`
+    // });
 
     gsap.from(".anm_heading_title_1 .char", 
         1, {
@@ -105,40 +105,149 @@ document.addEventListener("DOMContentLoaded", function () {
                 scrub: true 
             },  
         });
-      
-        function sectionThree(){
-            const tlSectionThree = gsap.timeline({
-                scrollTrigger: {     
-                    trigger: "#three", 
-                    start: 'top+=350 center',    			
-                    toggleActions: "play none none reverse",
-                    //  markers: true,
-                    // scrub: 2            
-                }
-            });
-            tlSectionThree.from(".anm_cta_title", {
-                y: 30,
-                opacity: 0
-                
-            });
-            tlSectionThree.from(".anm_cta_btn", {
-                y: 50,
-                opacity: 0,
-                delay: 1          
-            },'<');
-            tlSectionThree.to(".anm_cta_title", {
-                y: -20,       
-            },'<');
-            
-          }
-          sectionThree();
+    
+    function sectionTwo() {
+        gsap.from(".partners", {
+            opacity: 0, 
+            y: 50,
+            scrollTrigger: {
+                trigger: "#two",
+                start: "top+=150 center",
+                toggleActions: "play none none reverse",
+                //  markers: true
+            }
+        })
+    };
+    sectionTwo();
 
+    function sectionThree(){
+        const tlSectionThree = gsap.timeline({
+            scrollTrigger: {     
+                trigger: "#three", 
+                start: 'top+=150 center',    			
+                toggleActions: "play none none reverse",
+                //  markers: true,
+                // scrub: 2            
+            }
+        });
+        tlSectionThree.from(".anm_cta_title", {
+            y: 30,
+            opacity: 0
+            
+        });
+        tlSectionThree.from(".anm_cta_btn", {
+            y: 50,
+            opacity: 0,
+            delay: 1          
+        },'<');
+        tlSectionThree.to(".anm_cta_title", {
+            y: -20,       
+        },'<');
+        
+        };
+    sectionThree();
+
+    function sectionFour() {
+        gsap.from(".box", {
+            opacity: 0, 
+            y: 50,
+            stagger: 0.5,
+            scrollTrigger: {
+                trigger: "#four",
+                start: "top+=150 center",
+                toggleActions: "play none none reverse",
+                //  markers: true
+            }
+        })
+    };
+    sectionFour();
+
+    function sectionFive() {       
+        const tlSectionFive = gsap.timeline({
+            scrollTrigger: {     
+                trigger: "#five", 
+                start: 'top+=150 center',    			
+                toggleActions: "play none none reverse",
+                //  markers: true,
+                // scrub: 2            
+            }
+        });
+        tlSectionFive.from(".five_item_animation", {
+            opacity: 0, 
+            y: 50,
+            stagger: 0.5
+        })
+        tlSectionFive.from(".img-insights-desk", {
+            opacity: 0, 
+            x: 100,
+            scrollTrigger: {
+                trigger: "#five",                
+                toggleActions: "play none none reverse",
+                scrub: true 
+            }  
+        },'<');
+
+    };
+    sectionFive();
+
+    function sectionSix() {
+        gsap.from(".six_animation", {
+            opacity: 0, 
+            y: 50,
+            stagger: 0.5,
+            scrollTrigger: {
+                trigger: "#six",
+                start: "top+=150 center",
+                toggleActions: "play none none reverse",
+                //  markers: true
+            }
+        })
+    };
+    sectionSix();
+
+
+    function sectionForm() {       
+        gsap.from(".form_animation", {
+            opacity: 0, 
+            y: 50,
+            stagger: 0.5,
+            scrollTrigger: {
+                trigger: "#form",
+                start: "top+=150 center",
+                toggleActions: "play none none reverse",
+                //  markers: true
+            }
+        })
+    };
+    sectionForm();
+
+    function sectionFooter() {       
+        gsap.from(".footer-column", {
+            opacity: 0, 
+            y: 50,
+            stagger: 0.2,
+            scrollTrigger: {
+                trigger: "#footer",
+                start: "top center",
+                toggleActions: "play none none reverse",
+                //  markers: true
+            }
+        })
+    };
+    sectionFooter();
+
+    
 
 	
     let mm = gsap.matchMedia();
 
     mm.add("(min-width: 800px)", () => {
             // desktop setup code here...
+            gsap.set('.anm_heading_title_2', {
+                opacity: 1,
+                y: `${+innerHeight}px`
+            });
+
             tlHomeHero.to(".header-logo-box", {          
                 background: "linear-gradient(0deg, rgba(0, 0, 0, 0) -9%, rgba(0, 0, 0, 1) 154%)",               
             });   
@@ -147,6 +256,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     mm.add("(max-width: 799px)", () => {
             // mobile setup code here...
+            gsap.set('.anm_heading_title_2', {
+                opacity: 1,
+                y: `${+innerHeight}px`
+            });
+            gsap.set('#one.parallax__layer_hero', {                
+                height: `${+innerHeight * 2}px`
+            });
+            gsap.set('.image_three', {                
+                  backgroundPositionX: `30%`,
+            });
+          
+            
             tlHomeHero.to(".header-logo-box", {          
                 background: "linear-gradient(0deg, rgba(0, 0, 0, 0) -9%, rgba(0, 0, 0, 1) 154%)",               
             })
